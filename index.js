fb620d53f6c0d3096415fb18ed25104770994ef1
Shayan <162302193+Shayan123458@users.noreply.github.com>
2024-03-20 11:57:39 +0500

    Add files via upload
    a simple calculator

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+// Asking Questions from Users through inquirer
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "FirstNumber" },
+    { message: "Enter Second Number", type: "number", name: "SecondNumber" },
+    {
+        message: "Select one operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// Conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
